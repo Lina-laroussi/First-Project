95361ba60d9bcd3a98f0013151300976924ee81b
Lina Laroussi <laroussilina056@gmail.com>
2022-09-27 20:30:25 +0100

    first project symfony and controller

---
+++ FirstProject/src/Controller/TeacherController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    #[Route('/teacher/{name}', name: 'app_teacher')]
+    public function teacher($name): Response
+    {
+        return $this->render('teacher/index.html.twig', [
+            't' => $name,
+        ]);
+    }
+}
